7379ba6bd555553bbc7659280da18a983618443d
Darkshot12 <dhrumil10@icloud.com>
2024-10-17 22:12:09 -0400

    aaaaaa

--- train.py
+++ train.py
@@ -11,6 +11,7 @@ from stable_baselines3.common.callbacks import BaseCallback
 
 import time
 import matplotlib.pyplot as plt
+import os
 
 from gym_utils import SMBRamWrapper
 
@@ -26,10 +27,78 @@ n_stack = 4
 n_skip = 4
 
 env_wrap = SMBRamWrapper(env, [x0, x1, y0, y1], n_stack=n_stack, n_skip=n_skip)
-
 # test env_wrap
 done = True
 for i in range(150):
     if done:
         state = env_wrap.reset()
     state, reward, done, info = env_wrap.step(env_wrap.action_space.sample())
+state.shape
+
+fig, ax = plt.subplots(1, n_stack, figsize=(14,10))
+for i in range(n_stack):
+    ax[i].imshow(state[:,:,n_stack-i-1], vmin=-1, vmax=2)
+plt.show()
+
+# Apply other wrapper functions
+env_wrap = Monitor(env_wrap)  # for tensorboard log
+env_wrap = DummyVecEnv([lambda: env_wrap])
+
+# Callbacks
+
+# Save intermediate models
+# Copied from Nicholas Renotte's code
+class TrainAndLoggingCallback(BaseCallback):
+
+    def __init__(self, check_freq, save_path, 
+                 starting_steps=0, verbose=1):
+        super(TrainAndLoggingCallback, self).__init__(verbose)
+        self.check_freq = check_freq
+        self.save_path = save_path
+        self.starting_steps = starting_steps
+
+    def _init_callback(self):
+        if self.save_path is not None:
+            os.makedirs(self.save_path, exist_ok=True)
+
+    def _on_step(self):
+        if self.n_calls % self.check_freq == 0:
+            model_path = os.path.join(self.save_path, 'best_model_{}'.format(self.n_calls + int(self.starting_steps)))
+            self.model.save(model_path)
+
+        return True
+    
+# Linear learning rate schedule
+# https://stable-baselines3.readthedocs.io/en/master/guide/examples.html#learning-rate-schedule
+from typing import Callable
+
+def linear_schedule(initial_value: float) -> Callable[[float], float]:
+    """
+    Linear learning rate schedule.
+
+    :param initial_value: Initial learning rate.
+    :return: schedule that computes
+      current learning rate depending on remaining progress
+    """
+    def func(progress_remaining: float) -> float:
+        """
+        Progress will decrease from 1 (beginning) to 0.
+
+        :param progress_remaining:
+        :return: current learning rate
+        """
+        return progress_remaining * initial_value
+
+    return func
+### MODIFY THESE TWO DIRECTORIES BEFORE TRAINING A NEW MODEL ###
+MODEL_DIR = './models/NEW_MODEL_DIR'
+LOG_DIR = './logs/NEW_LOG_DIR'
+model = PPO('MlpPolicy', env_wrap, verbose=1, learning_rate=linear_schedule(3e-4), tensorboard_log=LOG_DIR) 
+callback = TrainAndLoggingCallback(check_freq=1e5, starting_steps=0, save_path=MODEL_DIR)
+
+t_start = time.time()
+
+model.learn(total_timesteps=10e6, callback=callback)
+
+t_elapsed = time.time() - t_start
+print('Wall time: {} s'.format(round(t_elapsed, 2)))
